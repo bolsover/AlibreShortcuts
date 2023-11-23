1c3170a480c5876608317e3c2d3cec0d6f563552
David Bolsover <david@bolsover.com>
2023-11-23 12:50:23 +0000

    Update SetupScript.iss to version 3.1

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -26,58 +26,58 @@ namespace UnitTests
         private StringBuilder sb = new();
 
 
-        [Test]
-        public void TestProfile()
-        {
-            string[] arguments = new[] { "D:/Repository/Jetbrains/Bolsover/UtilitiesForAlibre/User.NET.profile_27" };
-
-            LinearMap mapping;
-            {
-                FileStream fs = null;
-                try
-                {
-                    if (arguments.Length != 1)
-                        throw new Exception("usage: Profile <profile-file-name>");
-
-
-                    fs = new FileStream(arguments[0], FileMode.Open);
-                    Profile o = (Profile)ReadObjectFromFile(fs);
-                    mapping = o.Mapping;
-
-                    for (int i = 0; i < mapping.Pairs.Length; i++)
-                    {
-                        com.alibre.xml.PairXmlWrapper mappingPair = mapping.Pairs[i];
-
-                        var first = mappingPair.toWrappedObject().first;
-                        var second = mappingPair.toWrappedObject().second;
-
-
-                        string child = first.ToString();
-
-
-                        if (second is Profile)
-                        {
-                            Profile p = (Profile)second;
-                            DumpProfile(p, child);
-                        }
-                    }
-
-                    PrintToText(sb.ToString());
-                    String html = AddHtmlHeaderFooter(ConvertCsvToHtmlTable(sb.ToString()));
-                    //io.WriteLine(html);
-                    PrintToHtml(html);
-                }
-
-                catch (Exception ex)
-                {
-                    io.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    if (fs != null) fs.Close();
-                }
-            }
-        }
+        // [Test]
+        // public void TestProfile()
+        // {
+        //     string[] arguments = new[] { "D:/Repository/Jetbrains/Bolsover/UtilitiesForAlibre/User.NET.profile_27" };
+        //
+        //     LinearMap mapping;
+        //     {
+        //         FileStream fs = null;
+        //         try
+        //         {
+        //             if (arguments.Length != 1)
+        //                 throw new Exception("usage: Profile <profile-file-name>");
+        //
+        //
+        //             fs = new FileStream(arguments[0], FileMode.Open);
+        //             Profile o = (Profile)ReadObjectFromFile(fs);
+        //             mapping = o.Mapping;
+        //
+        //             for (int i = 0; i < mapping.Pairs.Length; i++)
+        //             {
+        //                 com.alibre.xml.PairXmlWrapper mappingPair = mapping.Pairs[i];
+        //
+        //                 var first = mappingPair.toWrappedObject().first;
+        //                 var second = mappingPair.toWrappedObject().second;
+        //
+        //
+        //                 string child = first.ToString();
+        //
+        //
+        //                 if (second is Profile)
+        //                 {
+        //                     Profile p = (Profile)second;
+        //                     DumpProfile(p, child);
+        //                 }
+        //             }
+        //
+        //             PrintToText(sb.ToString());
+        //             String html = AddHtmlHeaderFooter(ConvertCsvToHtmlTable(sb.ToString()));
+        //             //io.WriteLine(html);
+        //             PrintToHtml(html);
+        //         }
+        //
+        //         catch (Exception ex)
+        //         {
+        //             io.WriteLine(ex.Message);
+        //         }
+        //         finally
+        //         {
+        //             if (fs != null) fs.Close();
+        //         }
+        //     }
+        // }
 
         private string ConvertCsvToHtmlTable(string csvData)
         {
@@ -125,78 +125,78 @@ namespace UnitTests
             return obj;
         }
 
-        private void DumpProfile(Profile profile, string parent)
-        {
-            LinearMap mapping = profile.Mapping;
-
-
-            for (int i = 0; i < mapping.Pairs.Length; i++)
-            {
-                com.alibre.xml.PairXmlWrapper mappingPair = mapping.Pairs[i];
-
-                var first = mappingPair.toWrappedObject().first;
-                var second = mappingPair.toWrappedObject().second;
-
-
-                string child = parent + ", " + first;
-                KeysConverter kc = new KeysConverter();
-                string keyChar = kc.ConvertToString(second);
-                if (child.ToUpper().Contains("SHORTCUTS") && !second.ToString().ToUpper().Contains("PROFILE"))
-                {
-                    string toRemove = "SHORTCUTS,";
-                    child = child.Remove(child.IndexOf(toRemove), toRemove.Length);
-
-                    toRemove = first.ToString();
-                    string replace = LString.getLocalizedString(first.ToString(), LStringToken.ToolbarHint);
-                    if (replace is null)
-                    {
-                        replace = first.ToString();
-                    }
-
-                    replace = replace.Replace(",", " ");
-                    child = child.Replace(toRemove, replace);
-
-
-                    io.WriteLine(child + ", " + keyChar);
-                    sb.Append(child + ", " + keyChar + "\n");
-                }
-
-
-                if (mappingPair.toWrappedObject().second is Profile)
-                {
-                    Profile p = (Profile)mappingPair.toWrappedObject().second;
-                    DumpProfile(p, child);
-                }
-            }
-        }
-
-        private void PrintToText(string text)
-        {
-            try
-            {
-                StreamWriter sw = new StreamWriter(filePath);
-                sw.Write(text);
-                sw.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void PrintToHtml(string html)
-        {
-            try
-            {
-                StreamWriter sw = new StreamWriter(htmlFilePath);
-                sw.Write(html);
-                sw.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+        // private void DumpProfile(Profile profile, string parent)
+        // {
+        //     LinearMap mapping = profile.Mapping;
+        //
+        //
+        //     for (int i = 0; i < mapping.Pairs.Length; i++)
+        //     {
+        //         com.alibre.xml.PairXmlWrapper mappingPair = mapping.Pairs[i];
+        //
+        //         var first = mappingPair.toWrappedObject().first;
+        //         var second = mappingPair.toWrappedObject().second;
+        //
+        //
+        //         string child = parent + ", " + first;
+        //         KeysConverter kc = new KeysConverter();
+        //         string keyChar = kc.ConvertToString(second);
+        //         if (child.ToUpper().Contains("SHORTCUTS") && !second.ToString().ToUpper().Contains("PROFILE"))
+        //         {
+        //             string toRemove = "SHORTCUTS,";
+        //             child = child.Remove(child.IndexOf(toRemove), toRemove.Length);
+        //
+        //             toRemove = first.ToString();
+        //             string replace = LString.getLocalizedString(first.ToString(), LStringToken.ToolbarHint);
+        //             if (replace is null)
+        //             {
+        //                 replace = first.ToString();
+        //             }
+        //
+        //             replace = replace.Replace(",", " ");
+        //             child = child.Replace(toRemove, replace);
+        //
+        //
+        //             io.WriteLine(child + ", " + keyChar);
+        //             sb.Append(child + ", " + keyChar + "\n");
+        //         }
+        //
+        //
+        //         if (mappingPair.toWrappedObject().second is Profile)
+        //         {
+        //             Profile p = (Profile)mappingPair.toWrappedObject().second;
+        //             DumpProfile(p, child);
+        //         }
+        //     }
+        // }
+        //
+        // private void PrintToText(string text)
+        // {
+        //     try
+        //     {
+        //         StreamWriter sw = new StreamWriter(filePath);
+        //         sw.Write(text);
+        //         sw.Close();
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         MessageBox.Show(ex.Message);
+        //     }
+        // }
+        //
+        // private void PrintToHtml(string html)
+        // {
+        //     try
+        //     {
+        //         StreamWriter sw = new StreamWriter(htmlFilePath);
+        //         sw.Write(html);
+        //         sw.Close();
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         MessageBox.Show(ex.Message);
+        //     }
+        // }
 
         [Test]
         public void TestDefaultProfile()
